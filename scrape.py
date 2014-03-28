b383a48b4b74c327e7a0f735a865b01fc7d58bb8
Abe Stanway <abestanway@gmail.com>
2014-03-28 17:17:55 +0000

    use Ilya's githubarchive and fix loading bug

--- scrape.py
+++ scrape.py
@@ -13,54 +13,59 @@ from twitter_auth import token, token_secret, consumer, consumer_secret
 from twitter import Twitter, OAuth
 import subprocess
 import json
+import time
 import dateutil.parser
 
-cursor = dbauth.db.cursor()
-t = Twitter(
-    auth=OAuth(token, token_secret, consumer, consumer_secret))
-
-output = json.loads(subprocess.Popen(["ruby", "get_commits.rb"], stdout=subprocess.PIPE).communicate()[0])
-
 def insult(name):
-  person = '@' + name
-  i = ['Watch your mouth, ' + person, 
-       person + ', I love it when you talk dirty', 
-       'Very disappointed with your language, ' + person, 
-       'Yo,' + person + ', quit cursing in your code!',
-       person + ', you should be ashamed for using this kind of language.',
-       person + ', you kiss your mother with that mouth?',
-       person.upper() + ' CURSES IN HIS !@*^ING CODE',
-       person + ', act professional and quit using bad words. Fucker.',
-       'Impressive vocabulary, ' + person,
-       person + ', fucking cursing in your code and shit.',
-       'Hope your manager doesn\'t see this, ' + person + '!'
-       ]
+    person = '@' + name
+    i = ['Watch your mouth, ' + person, 
+        person + ', I love it when you talk dirty', 
+        'Very disappointed with your language, ' + person, 
+        'Yo,' + person + ', quit cursing in your code!',
+        person + ', you should be ashamed for using this kind of language.',
+        person + ', you kiss your mother with that mouth?',
+        person.upper() + ' CURSES IN HIS !@*^ING CODE',
+        person + ', act professional and quit using bad words. Fucker.',
+        'Impressive vocabulary, ' + person,
+        person + ', fucking cursing in your code and shit.',
+        'Hope your manager doesn\'t see this, ' + person + '!'
+        ]
 
-  return random.choice(i)
+    return random.choice(i)
 
 def find_avatar(userurl):
-  r = urllib2.urlopen(userurl)
-  body = r.read()
-  soup = BeautifulSoup(body)
-  for img in soup.find_all("img"):
-    print img
-    try:
-      if img.get('src').index('avatar') > 0:
-        return img.get('src')
-    except:
-      continue
+    r = urllib2.urlopen(userurl)
+    body = r.read()
+    soup = BeautifulSoup(body)
+    for img in soup.find_all("img"):
+        print img
+        try:
+            if img.get('src').index('avatar') > 0:
+                return img.get('src')
+        except:
+            continue
 
 
 def process(output):
-  for row in output:
-    try:
-        userurl = "https://" + row['userurl']
-        avatar = find_avatar(userurl)
-        created_at = dbauth.db.escape_string(dateutil.parser.parse(row['created_at']).strftime('%Y-%m-%d %H:%M:%S'))
-        query = "INSERT INTO new_commits VALUES ('', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', '')" % (row['commiter'], dbauth.db.escape_string(row['message']), avatar, dbauth.db.escape_string(row['commiturl']), dbauth.db.escape_string(userurl), created_at)
-        cursor.execute(query)
-    except Exception as e:
-        print e
-        continue
+    for row in output:
+        try:
+            userurl = "https://" + row['userurl']
+            avatar = find_avatar(userurl)
+            created_at = dbauth.db.escape_string(dateutil.parser.parse(row['created_at']).strftime('%Y-%m-%d %H:%M:%S'))
+            query = "INSERT INTO new_commits VALUES ('', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', '')" % (row['commiter'], dbauth.db.escape_string(row['message']), avatar, dbauth.db.escape_string(row['commiturl']), dbauth.db.escape_string(userurl), created_at)
+            cursor.execute(query)
+        except Exception as e:
+            print e
+            continue
 
-process(output)
+while True:
+    cursor = dbauth.db.cursor()
+    t = Twitter(
+        auth=OAuth(token, token_secret, consumer, consumer_secret))
+
+    output = subprocess.Popen(["/home/abasababa/.rvm/rubies/ruby-2.1.1/bin/ruby", "/home/abasababa/webapps/commit/get_commits.rb"], stdout=subprocess.PIPE).communicate()[0]
+    output = json.loads(output)
+
+    process(output)
+
+    time.sleep(3600)
