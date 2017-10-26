cabaed215dc22af70bc9a0cc8a7d9e8e1f336a94
Capuccino <sr229@users.noreply.github.com>
2017-10-26 22:35:32 +0800

    fix this quote
    heck

--- word_list.py
+++ word_list.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-word_list = [" fuck ", " bitch ", " shit ", " tits ", " asshole ", " arsehole ", " cocksucker", " cunt", " hell ", " douche", " testicle", " twat", " bastard", " sperm", " shit", " dildo", " wanker", " prick", " penis", " vagina", " whore", " boner", "wank", "wanker", "fag", "faggot ]
+word_list = [" fuck ", " bitch ", " shit ", " tits ", " asshole ", " arsehole ", " cocksucker", " cunt", " hell ", " douche", " testicle", " twat", " bastard", " sperm", " shit", " dildo", " wanker", " prick", " penis", " vagina", " whore", " boner", "wank", "wanker", "fag", "faggot" ]
